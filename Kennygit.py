c1770c62129e7fd9c69bc41f55688ed1e513f2dd
Pravgitadm <PRAVEEN43ECE@GMAIL.COM>
2024-06-26 12:10:07 +0530

    changed huuuu.txt file again in Copy45

---
+++ Kennygit.py
@@ -0,0 +1,1 @@
+this is phyton test file and please ignore the file
--- sree.py
+++ sree.py
@@ -1,5 +1,1 @@
-<<<<<<< HEAD
-this is aryaa
-=======
-this is aryaa
->>>>>>> b3e1e683a147ae469cdb1989c8669c1918609d4e
+this is sree
